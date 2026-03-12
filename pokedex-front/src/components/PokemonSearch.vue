eb7912e85cd8b932fed06c97f193f04a8102554e
Gustavo Vinicius Cristofolini <gustavo_cristofolini@econeteditora.com.br>
2026-03-12 09:00:00 -0300

    feat(002): melhorando layout da listagem

--- pokedex-front/src/components/PokemonSearch.vue
+++ pokedex-front/src/components/PokemonSearch.vue
@@ -6,20 +6,27 @@
       placeholder="Buscar pokemon..."
     />
 
-    <ul>
-        <li v-for="pokemon in results" :key="pokemon.name">
+  <div class="pokemon-grid">
 
-        <img
-            :src="getImage(pokemon.url)"
-            width="50"
-            @error="setDefault"
+    <div
+      v-for="pokemon in results"
+      :key="pokemon.name"
+      class="pokemon-card"
+    >
 
-        />
+      <img
+        :src="getImage(pokemon.url)"
+        width="80"
+        @error="setDefault"
+      />
 
-        {{ pokemon.name }}
+      <div class="pokemon-name">
+        {{ pokemon.name.replaceAll('-', ' ').replace(/\b\w/g, l => l.toUpperCase()) }}
+      </div>
 
-        </li>
-    </ul>
+    </div>
+
+  </div>
   </div>
 </template>
 
@@ -56,3 +63,34 @@ function setDefault(event){
   event.target.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png'
 }
 </script>
+<style scoped>
+
+.pokemon-grid{
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(120px,1fr));
+  gap: 15px;
+  margin-top: 20px;
+}
+
+.pokemon-card{
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  background: white;
+  border-radius: 10px;
+  padding: 15px;
+  text-align: center;
+  box-shadow: 0 3px 8px rgba(0,0,0,0.15);
+  cursor: pointer;
+  transition: transform 0.2s;
+}
+
+.pokemon-card:hover{
+  transform: scale(1.05);
+}
+
+.pokemon-name{
+  margin-top: 8px;
+  font-weight: bold;
+  text-transform: capitalize;
+}
+
+</style>
